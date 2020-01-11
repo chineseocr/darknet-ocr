df9d4fad3bff3523ca8133580c6412157c386fd0
chineseocr <chineseocr@hotmail.com>
2020-01-11 16:55:46 +0800

    新增10K中文模型，准确率92.1%

--- config.py
+++ config.py
@@ -4,12 +4,12 @@
 config
 @author: chineseocr
 """
-ocrType = 'chinese-5k'
+ocrType = 'chinese-relu'
 ocrPath  = 'models/ocr/{}/ocr.weights'.format(ocrType)
 textPath = 'models/text/text.weights'
 darkRoot ='darknet/libdarknet.so' ##darknet 
-TEXT_LINE_SCORE=0.85##text line prob
-scale = 600##可动态修改 no care text.cfg height,width
+TEXT_LINE_SCORE=0.7##text line prob
+scale = 900##可动态修改 no care text.cfg height,width
 maxScale = 1800
 GPU=True ## gpu for darknet  or cpu for opencv.dnn 
 anchors = '16,11, 16,16, 16,23, 16,33, 16,48, 16,68, 16,97, 16,139, 16,198, 16,283'
